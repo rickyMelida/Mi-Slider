8207068c428770c9c078fd1b1d99c8db8b2d1ab6
Ricardo Mélida <ricardomelida92@gmail.com>
2019-04-25 17:11:10 -0400

    Se simplifica codigo css

--- design.js
+++ design.js
@@ -1,5 +1,6 @@
 var img_1 = document.getElementById("slider_sec");
 function mover() {
-	img_1.style.width = "250%";
+	var imagrn = $("#slider_princ")
+	//img_1.style.width = "250%";
 	//alert(img_1.length);
 }
