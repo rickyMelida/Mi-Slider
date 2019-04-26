a9b51fdb7bc0bb6285aa12a0324889c38457167a
Ricardo Mélida <ricardomelida92@gmail.com>
2019-04-26 17:21:37 -0400

    Se logra que las imagenes vayan a la izquierda

--- design.js
+++ design.js
@@ -3,9 +3,18 @@ var imagen = $("#img_1");
 
 $("#slider_sec").css("margin-left", "-" + 100 + "%");
 
-$("input").click(function() {
+/*Va a la Derecha*/
+$("input[name=Derecha]").click(function() {
 	$("#slider_sec").animate({marginLeft: 0 + "%"}, 700, function() {
         $("#slider_sec .img:last").insertBefore("#slider_sec .img:first");
         $("#slider_sec").css("margin-left", "-" + 100 + "%");
 	});
+});
+
+/*Va a la Izquierda*/
+$("input[name=Izquierda]").click(function() {
+    $("#slider_sec").animate({marginLeft: "-" + 200 + "%"}, 700, function() {
+        $("#slider_sec .img:first").insertAfter("#slider_sec .img:last");
+        $("#slider_sec").css("margin-left", "-" + 100 + "%");
+    });
 });
