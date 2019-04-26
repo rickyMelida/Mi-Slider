bfa995d69c890bc3b77d308ad7f3b3b092dae6e7
Sub Estacion <subestacion@paseolagaleria.com>
2019-04-26 06:09:36 -0400

    Se modifican los estilos y archivo JS

--- design.js
+++ design.js
@@ -1,5 +1,11 @@
 var imagen = $("#img_1");
 
+//$("#slider_sec").css("margin-left", "-" + 100 + "%");
+
+$("#slider_sec").css("margin-left", "-" + 100 + "%");
+
 $("input").click(function() {
-	
+	$("#slider_sec").animate({marginLeft: 0 + "%"}, 700, function() {
+		$("#slider_sec .img:last").insertBefore("#slider_sec .img:first");
+	});
 });
