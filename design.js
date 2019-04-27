0566aaf04c067dc0d6e118c41c0d7a41d36a0118
Sub Estacion <subestacion@paseolagaleria.com>
2019-04-27 04:43:44 -0400

    Se hace automatico el slider

--- design.js
+++ design.js
@@ -18,3 +18,10 @@ $("input[name=Izquierda]").click(function() {
         $("#slider_sec").css("margin-left", "-" + 100 + "%");
     });
 });
+
+setInterval(function() {
+	$("#slider_sec").animate({marginLeft: 0 + "%"}, 2000, function() {
+        $("#slider_sec .img:last").insertBefore("#slider_sec .img:first");
+        $("#slider_sec").css("margin-left", "-" + 100 + "%");
+	});
+}, 4000);
